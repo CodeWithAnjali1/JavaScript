4bb8e39b33f3ba3b7afbbc2303511717c4e0fe2a
CodeWithAnjali1 <ishikaverma3185@gmail.com>
2025-07-24 17:33:39 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,10 @@
+let a = prompt("Enter first Number: ")
+let b = prompt("Enter second Number: ")
+let c = prompt("Enter third Number: ")
+
+let sum = Number(a) + Number(b) + Number(c)
+
+alert("Sum is: " + sum)
+
+alert(" Square of numbers: "+(a*a)+ " " +(b*b)+ " " +(c*c))
+
